91865999bbbabf0659cc21391f197f0c913d520b
Siam Hossain <siam.jes12@gmail.com>
2025-05-30 19:44:49 +0600

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -7,3 +8,15 @@ dotenv.config({
 )
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Error: ${error}`);
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection error", error);
+  });
